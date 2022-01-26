1dc18e998232eea883ac617a96987fbf0aec565a
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-25 22:02:35 -0500

    controller index.js  copy and paste from just tech news and update

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 class Comment extends Model {}
+
 Comment.init({
     id: {
         type: DataTypes.INTEGER,
