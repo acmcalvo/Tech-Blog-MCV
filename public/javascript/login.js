ec826138bb9d3288000610748d6611bd270639b9
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-26 22:29:00 -0500

    change api routes

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,14 +1,14 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (username && password) {
+  if (email && password) {
     const response = await fetch('/api/users/login', {
       method: 'post',
       body: JSON.stringify({
-        username,
+        email,
         password
       }),
       headers: { 'Content-Type': 'application/json' }
@@ -22,5 +22,4 @@ async function loginFormHandler(event) {
   }
 }
 
-
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
