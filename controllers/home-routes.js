294a9de439991594231e0bc95a2ec405061e7acf
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-02-03 21:58:17 -0500

    fixes misspelling on line 20 comment to comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -61,6 +61,7 @@ router.get("/post/:id", (req, res) => {
         return;
       }
       const post = dbPostData.get({ plain: true });
+      console.log(post)
       res.render("single-post", {
         post,
         loggedIn: req.session.loggedIn,
