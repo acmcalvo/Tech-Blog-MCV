1f5d29a292b60ceda6a0d2fa2707b3ee3ad636d2
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-02-05 13:58:48 -0500

    WIP

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,33 +2,33 @@ const { Post } = require('../models');
 
 const postData = [
   {
-    title: 'Sequelize Data Types',
-    post_text: 'Sequelize has several built in, handy data types that you can use when you define a model.  The most commonly used for simple databases are INTEGER and STRING.',
+    title: 'Sample Tittle 1',
+    post_text: 'Consequat sit irure anim excepteur ipsum do irure occaecat. Non ullamco Lorem aliquip deserunt reprehenderit consectetur proident non non anim cupidatat. Ipsum nulla exercitation fugiat fugiat labore.',
     user_id: 1,
   },
   {
-    title: 'Handlebars Template Engine',
-    post_text: 'Handlebars is a very useful npm package that allows you to use templates in your code, making things more modular, re-usable, and maintainable.  For instance, you can set up a layout used for several pages with templates for a home page and a user dashboard.',
+    title: 'Sample Tittle 2',
+    post_text: 'Ex labore cillum do amet qui eu consequat enim incididunt commodo ad culpa mollit tempor. Culpa ipsum minim mollit aute cupidatat proident duis enim consectetur. Nulla duis ex culpa esse do amet excepteur proident ex adipisicing. Incididunt nisi nostrud aliqua in. Ullamco enim irure officia sit..',
     user_id: 2,
   },
   {
-    title: 'Handlebars Partials',
-    post_text: 'Partials are another handlebars feature where you can create a partial bit of code for something like post information, or for a comment. You can then use that partial whenever that bit of information is needed for your website.',
+    title: 'Sample Tittle 3',
+    post_text: 'Tempor sunt Lorem et adipisicing veniam elit ut. Nulla voluptate aliquip aute irure laborum quis id laborum duis dolor proident. Lorem commodo ullamco dolor officia qui non adipisicing non eiusmod commodo aliquip commodo tempor.',
     user_id: 2,
   },
   {
-    title: 'Sessions',
-    post_text: 'When a user logs in, a session can be established using a package such as Express Session. A cookie will be saved on the user computer, authenticating them on the website. Cookies can be set for a limited time or indefinitely.',
+    title: 'Sample Tittle 4',
+    post_text: 'Qui consectetur occaecat aute tempor eu consectetur aliquip ipsum. Velit laboris laborum commodo pariatur laborum ex sit deserunt. Labore magna ut magna pariatur reprehenderit sunt deserunt. Exercitation nisi in Lorem sint. Minim deserunt quis fugiat pariatur.',
     user_id: 3,
   },
   {
-    title: 'Hashing',
-    post_text: 'A very important part of user authentication is hashing passwords. When a user creates a password and when they log in, sensitive information like the password should be protected. bcrypt is a popular npm package that accomplishes this task.',
+    title: 'Sample Tittle 5',
+    post_text: 'Ullamco est officia aliqua reprehenderit exercitation eiusmod ut. Aliquip dolor velit velit consequat deserunt consectetur aliquip occaecat est reprehenderit anim culpa. Elit aliquip qui elit deserunt cupidatat voluptate ad pariatur in amet mollit. Eiusmod ullamco proident reprehenderit laboris id eiusmod esse magna minim sit eu ea deserunt. Enim proident irure mollit officia non exercitation dolore est sit nulla proident proident ullamco. In elit incididunt dolor ea ut mollit ex dolore consectetur dolore deserunt nostrud sint. Tempor magna esse officia nisi ullamco aute aliquip ea nulla.',
     user_id: 4,
   },
   {
-    title: 'Express.js',
-    post_text: 'Express.js is an easy way to set up a server with JavaScript. With a few lines of code, you can be on your way to hosting a dynamic webpage.',
+    title: 'Sample Tittle 6',
+    post_text: 'Veniam esse deserunt elit quis deserunt cupidatat. Id consequat id cillum exercitation ex commodo ad culpa ad. Nisi nulla tempor est exercitation dolore qui duis sunt aute minim magna ut sit. Velit adipisicing minim pariatur enim et. Lorem proident non tempor officia nostrud ut cupidatat dolore sit ullamco anim.',
     user_id: 5,
   },
 ]
